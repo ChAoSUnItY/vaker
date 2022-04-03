fa73b96be15097e6edca585ed98f25becff66b6b
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-04-04 02:35:20 +0800

    refactor internet

--- vaker/internet.v
+++ vaker/internet.v
@@ -15,26 +15,23 @@ pub fn email(ptr PtrInfo) {
 	email_name := rand_string(7, &lb_eng)
 	email_domain := rand_string(7, &lb_eng)
 	email_tld := rand_element(vaker.tld)
-	pointer := &string(ptr.ptr)
 	unsafe {
-		*pointer = '$email_name@${email_domain}.$email_tld'
+		cast_assign<string>(ptr, '$email_name@${email_domain}.$email_tld')
 	}
 }
 
 [inline]
 pub fn mac_address(ptr PtrInfo) {
-	pointer := &string(ptr.ptr)
 	unsafe {
-		*pointer = insert_sep(rand.hex(12), ':', 2)
+		cast_assign<string>(ptr, insert_sep(rand.hex(12), ':', 2))
 	}
 }
 
 [inline]
 pub fn domain_name(ptr PtrInfo) {
 	domain_tld := rand_element(vaker.tld)
-	pointer := &string(ptr.ptr)
 	unsafe {
-		*pointer = '${rand_string(7, &lb_eng)}.$domain_tld'
+		cast_assign<string>(ptr, '${rand_string(7, &lb_eng)}.$domain_tld')
 	}
 }
 
@@ -44,58 +41,52 @@ pub fn url(ptr PtrInfo) {
 	count := format.count('%s')
 	domain_tld := rand_element(vaker.tld)
 	domain_name := '${rand_string(7, &lb_eng)}.$domain_tld'
-	pointer := &string(ptr.ptr)
 
 	if count == 1 {
 		unsafe {
-			*pointer = strconv.v_sprintf(format, domain_name)
+			cast_assign<string>(ptr, strconv.v_sprintf(format, domain_name))
 		}
 	} else {
 		user_name := rand_string(7, &lb_eng)
 
 		unsafe {
-			*pointer = strconv.v_sprintf(format, domain_name, user_name)
+			cast_assign<string>(ptr, strconv.v_sprintf(format, domain_name, user_name))
 		}
 	}
 }
 
 [inline]
 pub fn user_name(ptr PtrInfo) {
-	pointer := &string(ptr.ptr)
 	unsafe {
-		*pointer = rand_string(7, &lb_eng)
+		cast_assign<string>(ptr, rand_string(7, &lb_eng))
 	}
 }
 
 [inline]
 pub fn ipv4(ptr PtrInfo) {
-	pointer := &string(ptr.ptr)
 	unsafe {
-		*pointer = insert_sep(rand_digit_string(8), '.', 2)
+		cast_assign<string>(ptr, insert_sep(rand_digit_string(8), '.', 2))
 	}
 }
 
 [inline]
 pub fn ipv6(ptr PtrInfo) {
-	pointer := &string(ptr.ptr)
 	unsafe {
-		*pointer = insert_sep(rand.hex(32), ':', 4)
+		cast_assign<string>(ptr, insert_sep(rand.hex(32), ':', 4))
 	}
 }
 
 [inline]
 pub fn password(ptr PtrInfo) {
-	pointer := &string(ptr.ptr)
 	unsafe {
-		*pointer = rand_string(50, &lb_eng)
+		cast_assign<string>(ptr, rand_string(50, &lb_eng))
 	}
 }
 
 [inline]
 pub fn jwt(ptr PtrInfo) {
 	element := rand_string(40, &lb_eng)
-	pointer := &string(ptr.ptr)
 	unsafe {
-		*pointer = '${element}.${element}.$element'
+		cast_assign<string>(ptr, '${element}.${element}.$element')
 	}
 }
